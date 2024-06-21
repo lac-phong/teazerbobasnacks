6045821ac0c1b6f147223b6c9d0264109d979406
lac-phong <quanglac03@gmail.com>
2024-06-21 15:17:42 -0700

    established pages for shops

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {
+  BanhMiLogo,
+  BanhMiQR,
+  DumplingCartLogo,
+  DumplingCartQR,
+  TeazerLogo,
+  TeazerQR
+} from './assets';
 
 function App() {
+
+  const handleClick = (url) => {
+    window.open(url, '_blank');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={BanhMiLogo} alt="Banh Mi Logo" className="fixed-size" onClick={() => handleClick('https://www.banhmibowlsc.com/')}/>
+      <img src={BanhMiQR} alt="Banh Mi QR" className="fixed-size" />
+      <br></br>
+      <img src={DumplingCartLogo} alt="Dumpling Cart Logo" className="fixed-size" onClick={() => handleClick('https://sjdowntownfoodhall.com/s/dumpling-cart/96-e-santa-clara-st-san-jose/d8e1b502-8fac-42f3-b79c-048414706367/Lychee%20Green%20Tea/dae8de00-4d04-4e77-bd66-a8e3c2dbe078')}/>
+      <img src={DumplingCartQR} alt="Dumpling Cart QR" className="fixed-size" />
+      <br></br>
+      <img src={TeazerLogo} alt="Teazer Logo" className="fixed-size" onClick={() => handleClick('https://sjdowntownfoodhall.com/s/teazer/96-e-santa-clara-st-san-jose/3e9580fb-4fca-4161-98c1-a241752b54b8')}/>
+      <img src={TeazerQR} alt="Teazer QR" className="fixed-size" />
     </div>
   );
 }
---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export const BanhMiLogo = require('./logos/banh_mi_logo.png');
+export const DumplingCartLogo = require('./logos/dc_logo.jpg');
+export const TeazerLogo = require('./logos/teazer_logo.jpg');
+export const BanhMiQR = require('./qrs/banh_mi_qr.png');
+export const DumplingCartQR = require('./qrs/dc_qr.png');
+export const TeazerQR = require('./qrs/teazer_qr.png');
